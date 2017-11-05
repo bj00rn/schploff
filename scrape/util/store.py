3490d8138135d7dcaf0099d802c16427e6516ee2
bj00rn <bjorn.erik.lundgren@gmail.com>
2017-11-05 21:31:52 +0100

    clean up

--- scrape/util/store.py
+++ scrape/util/store.py
@@ -5,30 +5,30 @@ import sqlite3 as lite
 import sys
 
 class Store():
-   
-   def __init__(self, db_file):
-      self.con = lite.connect(db_file)
 
-   def __enter__(self):
-      return self
+    def __init__(self, db_file):
+        self.con = lite.connect(db_file)
 
-   def __exit__(self, exc_type, exc_val, exc_tb):
-      self.con.close()
+    def __enter__(self):
+        return self
 
-   def get(self, hash):
-      cur = self.con.cursor()
-      cur.execute('SELECT * from download where hash=?', (hash,))
-      data = cur.fetchone()
-      return data
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.con.close()
 
-   def add(self, hash, timestamp, filename):
-      cur = self.con.cursor()
-      cur.execute('insert into download values(?,?,?)', (hash, timestamp, filename,))
-      self.con.commit()
-      return True
- 
-   def remove(self, hash):
-       cur = self.con.cursor()
-       cur.execute("DELETE FROM download WHERE hash=?", (hash,))
-       self.con.commit()
-       return True
+    def get(self, hash):
+        cur = self.con.cursor()
+        cur.execute('SELECT * from download where hash=?', (hash,))
+        data = cur.fetchone()
+        return data
+
+    def add(self, hash, timestamp, filename):
+        cur = self.con.cursor()
+        cur.execute('insert into download values(?,?,?)', (hash, timestamp, filename,))
+        self.con.commit()
+        return True
+
+    def remove(self, hash):
+        cur = self.con.cursor()
+        cur.execute("DELETE FROM download WHERE hash=?", (hash,))
+        self.con.commit()
+        return True
