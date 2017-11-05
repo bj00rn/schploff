bc450fcecf91cd2c6bfed95b03b2fda8f0e977d8
bj00rn <bjorn.erik.lundgren@gmail.com>
2017-11-05 21:24:50 +0100

    remove junk

--- smhilog/import/import_sqlite.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS  imp(datum text, tid text, vindriktning varchar(10), kvalitet varchar(10) ,vindhastighet varchar(10), kvalitet2 varchar(10), station varchar(10) );
-
-.separator ,
-.import cleaned.csv imp
